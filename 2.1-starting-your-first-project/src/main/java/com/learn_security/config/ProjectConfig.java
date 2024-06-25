c855cb03e8f993f47439cb5593828a8d5ac352cf
Phuong Nguyen <phuongsover2@gmail.com>
2024-06-26 01:06:02 +0700

    Listing 2.6 Defining a SecurityFilterChain bean

--- 2.1-starting-your-first-project/src/main/java/com/learn_security/config/ProjectConfig.java
+++ 2.1-starting-your-first-project/src/main/java/com/learn_security/config/ProjectConfig.java
@@ -2,14 +2,22 @@ package com.learn_security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ProjectConfig {
+
+    @Bean
+    SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        return http.build();
+    }
+
     @Bean
     UserDetailsService userDetailsService() {
         var user = User.withUsername("phuong")
