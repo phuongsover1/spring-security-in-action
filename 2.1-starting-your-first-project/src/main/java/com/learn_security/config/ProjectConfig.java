b328ea7adf199dc353d3e514eb1a1b65b2c93d24
Phuong Nguyen <phuongsover2@gmail.com>
2024-06-26 00:53:46 +0700

    Listing 2.5 The full definition of the configuration class

--- 2.1-starting-your-first-project/src/main/java/com/learn_security/config/ProjectConfig.java
+++ 2.1-starting-your-first-project/src/main/java/com/learn_security/config/ProjectConfig.java
@@ -4,6 +4,8 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
@@ -15,5 +17,10 @@ public class ProjectConfig {
                 .authorities("read")
                 .build();
         return new InMemoryUserDetailsManager(user);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
