4b0159731df8272682f1ef7113674d54352a7eaf
Phuong Nguyen <phuongsover2@gmail.com>
2024-06-26 00:40:22 +0700

    Listing 2.3 The configuration class for the UserDetailsService bean

---
+++ 2.1-starting-your-first-project/src/main/java/com/learn_security/config/ProjectConfig.java
@@ -0,0 +1,14 @@
+package com.learn_security.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+
+@Configuration
+public class ProjectConfig {
+    @Bean
+    UserDetailsService userDetailsService() {
+        return new InMemoryUserDetailsManager();
+    }
+}
