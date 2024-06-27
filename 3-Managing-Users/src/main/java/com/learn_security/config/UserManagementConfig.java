5cc8ce938274713c60477dcec50aadd6402ea2ba
Phuong Nguyen <phuongsover2@gmail.com>
2024-06-28 00:04:33 +0700

    Listing 3.19 Registering the JdbcUserDetailsManager in the configuration class

--- 3-Managing-Users/src/main/java/com/learn_security/config/UserManagementConfig.java
+++ 3-Managing-Users/src/main/java/com/learn_security/config/UserManagementConfig.java
@@ -1,24 +1,20 @@
 package com.learn_security.config;
 
-import com.learn_security.config.services.InMemoryUserDetailsService;
-import com.learn_security.model.User;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 
-import java.util.List;
+import javax.sql.DataSource;
 
 @Configuration
 public class UserManagementConfig {
 
     @Bean
-    public UserDetailsService userDetailsService() {
-        List<UserDetails> users = List.of(new User("phuong", "12345", "READ"));
-        return new InMemoryUserDetailsService(users);
+    public UserDetailsService userDetailsService(DataSource dataSource) {
+        return new JdbcUserDetailsManager(dataSource);
 
     }
 
