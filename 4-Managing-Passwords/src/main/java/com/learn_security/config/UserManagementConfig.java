7d7e28425bef796b48f7315f9a4d0336f92e6117
Phuong Nguyen <phuongsover2@gmail.com>
2024-06-28 03:39:38 +0700

    4.1.4 Multiple encoding strategies with DelegatingPasswordEncoder

--- 4-Managing-Passwords/src/main/java/com/learn_security/config/UserManagementConfig.java
+++ 4-Managing-Passwords/src/main/java/com/learn_security/config/UserManagementConfig.java
@@ -1,6 +1,8 @@
 package com.learn_security.config;
 
 import com.learn_security.config.services.InMemoryUserDetailsService;
+import com.learn_security.config.services.PlainTextPasswordEncoder;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.core.userdetails.User;
@@ -11,11 +13,13 @@ import org.springframework.security.crypto.password.DelegatingPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.scrypt.SCryptPasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+@Slf4j
 @Configuration
 public class UserManagementConfig {
 
@@ -26,18 +30,27 @@ public class UserManagementConfig {
                 .accountLocked(false)
                 .credentialsExpired(false)
                 .disabled(false)
-                .authorities("READ")
-                .passwordEncoder(p -> passwordEncoder().encode(p));
+                .authorities("READ");
 
-        UserDetails u1 = userBuilder.username("phuong")
-                .password("123456")
+
+        UserDetails u1 = userBuilder.username("NoOpUser")
+                .password(new PlainTextPasswordEncoder().encode("{noop}noop_password"))
                 .build();
 
-        UserDetails u2 = userBuilder.username("john")
-                .password("12345")
+
+        UserDetails u3 = userBuilder.username("DefaultBcryptUser")
+                .password(passwordEncoder().encode("default_bcrypt_password"))
                 .build();
 
-        return new InMemoryUserDetailsService(List.of(u1, u2));
+        UserDetails u4 = userBuilder.username("ScryptUser")
+                .password("{scrypt}" + new SCryptPasswordEncoder(16384, 8, 1, 32, 64).encode("scrypt_password"))
+                .build();
+
+        log.info("NoOpUser's password: {}", u1.getPassword());
+        log.info("DefaultBcryptUser's password: {}", u3.getPassword());
+        log.info("ScryptUser's password: {}", u4.getPassword());
+
+        return new InMemoryUserDetailsManager(List.of(u1, u3, u4));
 
     }
 
@@ -46,7 +59,7 @@ public class UserManagementConfig {
         Map<String, PasswordEncoder> encoders = new HashMap<>();
 
         encoders.put("noop", NoOpPasswordEncoder.getInstance());
-        encoders.put("bcrypt", new BCryptPasswordEncoder(10)) ;
+        encoders.put("bcrypt", new BCryptPasswordEncoder(10));
         encoders.put("scrypt", new SCryptPasswordEncoder(16384, 8, 1, 32, 64));
 
         return new DelegatingPasswordEncoder("bcrypt", encoders);
