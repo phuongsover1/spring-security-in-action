9058be6add6987ef6565711b4279aafb9a56a19a
Phuong Nguyen <phuongsover2@gmail.com>
2024-06-28 22:48:11 +0700

    4.2.1 Returning the same key value for each call of the generateKey()

--- 4-Managing-Passwords/src/main/java/com/learn_security/crypto/SpringSecurityCrypto.java
+++ 4-Managing-Passwords/src/main/java/com/learn_security/crypto/SpringSecurityCrypto.java
@@ -14,7 +14,8 @@ public class SpringSecurityCrypto implements CommandLineRunner {
     public void run(String... args) throws Exception {
         // 4.2.1 Using key generators
 //         usingKeyGenerators();
-        using16ByteKeyGenerators();
+//        using16ByteKeyGenerators();
+        returnSameKeyValueForEachCallToGenerateKeyMethod();
     }
 
     private void usingKeyGenerators() {
@@ -35,6 +36,14 @@ public class SpringSecurityCrypto implements CommandLineRunner {
         int keyLength = keyGenerator.getKeyLength();
         log.info("Byte key: {}", key);
         log.info("Byte key's length: {}", keyLength);
+    }
+
+    private void returnSameKeyValueForEachCallToGenerateKeyMethod() {
+        BytesKeyGenerator keyGenerator = KeyGenerators.shared(16);
+        byte[] key1 = keyGenerator.generateKey();
+        byte[] key2 = keyGenerator.generateKey();
+        log.info("Key 1: {}", key1);
+        log.info("Key 2: {}", key2);
     }
 
 }
