5165f77ebad9730967821c18ff194c4e81df9cbc
Phuong Nguyen <phuongsover2@gmail.com>
2024-06-29 00:21:36 +0700

    Listing 5.1 Implementing a custom filter

---
+++ 5-A-web-app’s-security-begins-with-filters/src/main/java/com/learn_security/config/filter/RequestValidationFilter.java
@@ -0,0 +1,12 @@
+package com.learn_security.config.filter;
+
+import jakarta.servlet.*;
+
+import java.io.IOException;
+
+public class RequestValidationFilter implements Filter {
+    @Override
+    public void doFilter(ServletRequest servletRequest, ServletResponse servletResponse, FilterChain filterChain) throws IOException, ServletException {
+
+    }
+}
