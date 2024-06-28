c228fa06735c2bc1d3356f629cd4d66a7a9a8b28
Phuong Nguyen <phuongsover2@gmail.com>
2024-06-29 01:54:51 +0700

    Listing 5.5 Defining a filter to log requests

---
+++ 5-A-web-app’s-security-begins-with-filters/src/main/java/com/learn_security/config/filter/AuthenticationLoggingFilter.java
@@ -0,0 +1,22 @@
+package com.learn_security.config.filter;
+
+import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
+
+import java.io.IOException;
+import java.util.logging.Logger;
+
+public class AuthenticationLoggingFilter implements Filter {
+
+    private final Logger logger = Logger.getLogger(AuthenticationLoggingFilter.class.getName());
+    @Override
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain) throws IOException, ServletException {
+        var httpRequest = (HttpServletRequest) request;
+        var requestId = httpRequest.getHeader("Request-Id");
+
+        logger.info("Successfully authenticated request with id " + requestId);
+
+        filterChain.doFilter(request, response);
+
+    }
+}
--- 5-A-web-app’s-security-begins-with-filters/src/main/java/com/learn_security/config/filter/RequestValidationFilter.java
+++ 5-A-web-app’s-security-begins-with-filters/src/main/java/com/learn_security/config/filter/RequestValidationFilter.java
@@ -19,7 +19,7 @@ public class RequestValidationFilter implements Filter {
             return;
         }
 
-        filterChain.doFilter(httpRequest, httpResponse);
+        filterChain.doFilter(servletRequest, servletResponse);
 
     }
 }
