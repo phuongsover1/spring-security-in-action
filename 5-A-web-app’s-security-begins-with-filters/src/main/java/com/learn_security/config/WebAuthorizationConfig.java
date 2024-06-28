7ae01dd663585ca26509dcafbed777a109d64c1b
Phuong Nguyen <phuongsover2@gmail.com>
2024-06-29 00:31:34 +0700

    Listing 5.3 Configuring the custom filter before authentication

--- 5-A-web-app’s-security-begins-with-filters/src/main/java/com/learn_security/config/WebAuthorizationConfig.java
+++ 5-A-web-app’s-security-begins-with-filters/src/main/java/com/learn_security/config/WebAuthorizationConfig.java
@@ -1,17 +1,21 @@
 package com.learn_security.config;
 
+import com.learn_security.config.filter.RequestValidationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
 
 @Configuration
 public class WebAuthorizationConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.httpBasic(Customizer.withDefaults());
+        http.addFilterBefore(
+                new RequestValidationFilter(),
+                BasicAuthenticationFilter.class
+        );
 
         http.authorizeHttpRequests(c -> c.anyRequest().authenticated());
 
