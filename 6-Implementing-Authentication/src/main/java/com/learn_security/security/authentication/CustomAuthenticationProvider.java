3dc294663ddd2a30cf50f66a9c80033018ee9486
Phuong Nguyen <phuongsover2@gmail.com>
2024-06-29 15:37:49 +0700

    Listing 6.4 Implementing the authentication logic

--- 6-Implementing-Authentication/src/main/java/com/learn_security/security/authentication/CustomAuthenticationProvider.java
+++ 6-Implementing-Authentication/src/main/java/com/learn_security/security/authentication/CustomAuthenticationProvider.java
@@ -1,16 +1,36 @@
 package com.learn_security.security.authentication;
 
 import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
 @Component
 public class CustomAuthenticationProvider implements AuthenticationProvider {
+    private final UserDetailsService userDetailsService;
+    private final PasswordEncoder passwordEncoder;
+
+    public CustomAuthenticationProvider(UserDetailsService userDetailsService, PasswordEncoder passwordEncoder) {
+        this.userDetailsService = userDetailsService;
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        return null;
+        String username = authentication.getName();
+        String password = authentication.getCredentials().toString();
+
+        UserDetails u = userDetailsService.loadUserByUsername(username);
+
+        if (passwordEncoder.matches(password, u.getPassword()))
+            return new UsernamePasswordAuthenticationToken(username, password, u.getAuthorities());
+        else
+            throw new BadCredentialsException("Something went wrong!");
     }
 
     @Override
