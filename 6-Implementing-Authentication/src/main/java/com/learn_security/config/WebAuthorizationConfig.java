1c351b4f6607c7c4021ec02e6601c213f34e23aa
Phuong Nguyen <phuongsover2@gmail.com>
2024-06-29 16:03:23 +0700

    Listing 6.5 Registering the AuthenticationProvider in the configuration class

--- 6-Implementing-Authentication/src/main/java/com/learn_security/config/WebAuthorizationConfig.java
+++ 6-Implementing-Authentication/src/main/java/com/learn_security/config/WebAuthorizationConfig.java
@@ -2,16 +2,28 @@ package com.learn_security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class WebAuthorizationConfig {
 
+    private final AuthenticationProvider authenticationProvider;
+
+    public WebAuthorizationConfig(AuthenticationProvider authenticationProvider) {
+        this.authenticationProvider = authenticationProvider;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
-        http.authorizeHttpRequests(c -> c.anyRequest().permitAll());
+        http.httpBasic(Customizer.withDefaults());
+
+        http.authenticationProvider(authenticationProvider);
+
+        http.authorizeHttpRequests(c -> c.anyRequest().authenticated());
 
         return http.build();
     }
