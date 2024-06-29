99b2ecee2d6633ca671f0fca3cb665cd84704dc3
Phuong Nguyen <phuongsover2@gmail.com>
2024-06-29 15:22:18 +0700

    Listing 6.3 Overriding the supports() method of the AuthenticationProvider

---
+++ 6-Implementing-Authentication/src/main/java/com/learn_security/security/authentication/CustomAuthenticationProvider.java
@@ -0,0 +1,21 @@
+package com.learn_security.security.authentication;
+
+import org.springframework.security.authentication.AuthenticationProvider;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.stereotype.Component;
+
+@Component
+public class CustomAuthenticationProvider implements AuthenticationProvider {
+    @Override
+    public Authentication authenticate(Authentication authentication) throws AuthenticationException {
+        return null;
+    }
+
+    @Override
+    public boolean supports(Class<?> authenticationType) {
+        return authenticationType
+                .equals(UsernamePasswordAuthenticationToken.class);
+    }
+}
