635b76a2d99d08cccde1c7bb1c48c5143f7e481c
Phuong Nguyen <phuongsover2@gmail.com>
2024-06-29 21:26:48 +0700

    Listing 6.21 Setting a default success URL for the login form

--- 6-Implementing-Authentication-Form-Based/src/main/java/com/learn_security/config/ProjectConfig.java
+++ 6-Implementing-Authentication-Form-Based/src/main/java/com/learn_security/config/ProjectConfig.java
@@ -2,7 +2,6 @@ package com.learn_security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -11,7 +10,7 @@ public class ProjectConfig {
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
 
-        http.formLogin(Customizer.withDefaults());
+        http.formLogin(c -> c.defaultSuccessUrl("/home", true));
 
         http.authorizeHttpRequests(c -> c.anyRequest().authenticated());
         return http.build();
