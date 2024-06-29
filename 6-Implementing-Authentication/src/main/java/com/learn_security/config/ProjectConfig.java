c74512f7408bed1ac0ef78d2ac32e19d0b393359
Phuong Nguyen <phuongsover2@gmail.com>
2024-06-29 16:44:31 +0700

    Listing 6.9 An @Async method served by a different thread

---
+++ 6-Implementing-Authentication/src/main/java/com/learn_security/config/ProjectConfig.java
@@ -0,0 +1,9 @@
+package com.learn_security.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.EnableAsync;
+
+@Configuration
+@EnableAsync
+public class ProjectConfig {
+}
--- 6-Implementing-Authentication/src/main/java/com/learn_security/controllers/HomeController.java
+++ 6-Implementing-Authentication/src/main/java/com/learn_security/controllers/HomeController.java
@@ -1,5 +1,7 @@
 package com.learn_security.controllers;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.scheduling.annotation.Async;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContext;
 import org.springframework.security.core.context.SecurityContextHolder;
@@ -7,10 +9,19 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-public class HelloController {
+@Slf4j
+public class HomeController {
 
     @GetMapping("/hello")
     public String hello(Authentication a) {
         return "Hello!"  + a.getName() + "!";
+    }
+
+    @GetMapping("/bye")
+    @Async
+    public void goodbye() {
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        String username = securityContext.getAuthentication().getName();
+        log.info("username in goodbye method: {}", username);
     }
 }
