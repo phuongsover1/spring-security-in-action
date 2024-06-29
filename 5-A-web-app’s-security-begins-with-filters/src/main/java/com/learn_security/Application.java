84c35ca0076d26b1750470bf0a2db81c48cb3cf1
Phuong Nguyen <phuongsover2@gmail.com>
2024-06-29 11:58:30 +0700

    Disabling configuration of the default UserDetailsService

--- 5-A-web-app’s-security-begins-with-filters/src/main/java/com/learn_security/Application.java
+++ 5-A-web-app’s-security-begins-with-filters/src/main/java/com/learn_security/Application.java
@@ -2,12 +2,13 @@ package com.learn_security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
 
 }
