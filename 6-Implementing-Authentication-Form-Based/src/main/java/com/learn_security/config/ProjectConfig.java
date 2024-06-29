d6c2eab7c1ac61df5eaea40551bb285846847555
Phuong Nguyen <phuongsover2@gmail.com>
2024-06-29 20:36:38 +0700

    Listing 6.17 Implementing an AuthenticationEntryPoint  and setting customAuthenticationEntryPoint to customize the response for a failed authentication.

--- 6-Implementing-Authentication-Form-Based/src/main/java/com/learn_security/config/ProjectConfig.java
+++ 6-Implementing-Authentication-Form-Based/src/main/java/com/learn_security/config/ProjectConfig.java
@@ -2,7 +2,6 @@ package com.learn_security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -10,7 +9,12 @@ import org.springframework.security.web.SecurityFilterChain;
 public class ProjectConfig {
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        http.httpBasic(Customizer.withDefaults());
+        http.httpBasic(c -> {
+            c.realmName("OTHER");
+            c.authenticationEntryPoint(new CustomEntryPoint());
+        });
+
+        http.authorizeHttpRequests(c -> c.anyRequest().authenticated());
         return http.build();
     }
 
