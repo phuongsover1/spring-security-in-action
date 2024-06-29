2d4a02e8b86205d2e99aeddd0d59c9a01ddcb038
Phuong Nguyen <phuongsover2@gmail.com>
2024-06-29 21:17:01 +0700

    Listing 6.20 Defining the action method of the controller for the home.xhtml page

--- 6-Implementing-Authentication-Form-Based/src/main/java/com/learn_security/controllers/HomeController.java
+++ 6-Implementing-Authentication-Form-Based/src/main/java/com/learn_security/controllers/HomeController.java
@@ -1,26 +1,16 @@
 package com.learn_security.controllers;
 
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Async;
-import org.springframework.security.concurrent.DelegatingSecurityContextCallable;
-import org.springframework.security.concurrent.DelegatingSecurityContextExecutorService;
-import org.springframework.security.core.Authentication;
-import org.springframework.security.core.context.SecurityContext;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-import java.util.concurrent.Callable;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-
-@RestController
+@Controller
 @Slf4j
 public class HomeController {
 
-    @GetMapping("/hello")
-    public String hello(Authentication a) {
-        return "Hello!" + a.getName() + "!";
+    @GetMapping("/home")
+    public String hello() {
+        return "home.xhtml";
     }
 
 }
