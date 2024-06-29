9bfae2e015e4816064edde4f6f2a42aad9a6f05a
Phuong Nguyen <phuongsover2@gmail.com>
2024-06-29 17:53:07 +0700

    Defining an ExecutorService and submitting the task and Running the task decorated by DelegatingSecurityContextCallable

--- 6-Implementing-Authentication/src/main/java/com/learn_security/controllers/HomeController.java
+++ 6-Implementing-Authentication/src/main/java/com/learn_security/controllers/HomeController.java
@@ -8,13 +8,17 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.concurrent.Callable;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
 @RestController
 @Slf4j
 public class HomeController {
 
     @GetMapping("/hello")
     public String hello(Authentication a) {
-        return "Hello!"  + a.getName() + "!";
+        return "Hello!" + a.getName() + "!";
     }
 
     @GetMapping("/bye")
@@ -24,4 +28,21 @@ public class HomeController {
         String username = securityContext.getAuthentication().getName();
         log.info("username in goodbye method: {}", username);
     }
+
+    @GetMapping("/ciao")
+    public String ciao() throws Exception {
+        Callable<String> task = () -> {
+            log.info("Inside ExecutorService running task");
+            SecurityContext securityContext = SecurityContextHolder.getContext();
+            return securityContext.getAuthentication().getName();
+        };
+
+        ExecutorService e = Executors.newCachedThreadPool();
+        try {
+            return "Ciao, " + e.submit(task).get() + "!";
+        } finally {
+            e.shutdown();
+        }
+    }
+
 }
