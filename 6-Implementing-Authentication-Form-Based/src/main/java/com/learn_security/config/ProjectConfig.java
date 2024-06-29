c069eaa284a3838579ba9763fb166b7bd614cb66
Phuong Nguyen <phuongsover2@gmail.com>
2024-06-29 20:11:02 +0700

    Listing 6.15 Setting the HTTP Basic authentication method

--- 6-Implementing-Authentication-Form-Based/src/main/java/com/learn_security/config/ProjectConfig.java
+++ 6-Implementing-Authentication-Form-Based/src/main/java/com/learn_security/config/ProjectConfig.java
@@ -1,13 +1,17 @@
 package com.learn_security.config;
 
-import org.springframework.beans.factory.InitializingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.scheduling.annotation.EnableAsync;
-import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
-@EnableAsync
 public class ProjectConfig {
+    @Bean
+    public SecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http.httpBasic(Customizer.withDefaults());
+        return http.build();
+    }
 
 }
