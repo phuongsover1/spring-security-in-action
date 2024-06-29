27c6f706124442c6d12664c0dc59d8f035cf546a
Phuong Nguyen <phuongsover2@gmail.com>
2024-06-29 16:28:23 +0700

    Listing 6.7 Obtaining the SecurityContext from the SecurityContextHolder

--- 6-Implementing-Authentication/src/main/java/com/learn_security/controllers/HelloController.java
+++ 6-Implementing-Authentication/src/main/java/com/learn_security/controllers/HelloController.java
@@ -1,5 +1,8 @@
 package com.learn_security.controllers;
 
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -8,6 +11,9 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello() {
-        return "Hello!";
+
+        SecurityContext securityContext = SecurityContextHolder.getContext();
+        Authentication a = securityContext.getAuthentication();
+        return "Hello!"  + a.getName() + "!";
     }
 }
