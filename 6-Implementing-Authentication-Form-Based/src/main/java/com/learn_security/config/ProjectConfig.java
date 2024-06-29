da4034dcb6a3ceb1eea0a4cfb9271ad500bf62c4
Phuong Nguyen <phuongsover2@gmail.com>
2024-06-29 20:57:03 +0700

    Listing 6.19 Changing the authentication method to a form-based login

--- 6-Implementing-Authentication-Form-Based/src/main/java/com/learn_security/config/ProjectConfig.java
+++ 6-Implementing-Authentication-Form-Based/src/main/java/com/learn_security/config/ProjectConfig.java
@@ -2,6 +2,7 @@ package com.learn_security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -9,10 +10,8 @@ import org.springframework.security.web.SecurityFilterChain;
 public class ProjectConfig {
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
-        http.httpBasic(c -> {
-            c.realmName("OTHER");
-            c.authenticationEntryPoint(new CustomEntryPoint());
-        });
+
+        http.formLogin(Customizer.withDefaults());
 
         http.authorizeHttpRequests(c -> c.anyRequest().authenticated());
         return http.build();
