c7c17ab713e1b80033d8f2096f4caec51895d6cd
Phuong Nguyen <phuongsover2@gmail.com>
2024-06-30 15:14:19 +0700

    Listing 6.22 Implementing an AuthenticationSuccessHandler

--- 6-Implementing-Authentication-Form-Based/src/main/java/com/learn_security/config/ProjectConfig.java
+++ 6-Implementing-Authentication-Form-Based/src/main/java/com/learn_security/config/ProjectConfig.java
@@ -3,17 +3,55 @@ package com.learn_security.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.List;
 
 @Configuration
 public class ProjectConfig {
+    private final CustomAuthenticationSuccessHandler authenticationSuccessHandler;
+
+    public ProjectConfig(CustomAuthenticationSuccessHandler authenticationSuccessHandler) {
+        this.authenticationSuccessHandler = authenticationSuccessHandler;
+    }
+
     @Bean
     public SecurityFilterChain configure(HttpSecurity http) throws Exception {
 
-        http.formLogin(c -> c.defaultSuccessUrl("/home", true));
+        http.formLogin(c -> {
+            c.defaultSuccessUrl("/home", true);
+            c.successHandler(authenticationSuccessHandler);
+        });
 
         http.authorizeHttpRequests(c -> c.anyRequest().authenticated());
         return http.build();
+    }
+
+    @Bean
+    public UserDetailsService userDetailsService() {
+        User.UserBuilder builder = User.builder()
+                .disabled(false);
+        UserDetails u1 = builder.username("phuong")
+                .password("12345")
+                .authorities(List.of(() -> "READ"))
+                .build();
+
+        UserDetails u2 = builder.username("john")
+                .password("123456")
+                .authorities("READ", "WRITE")
+                .build();
+        return new InMemoryUserDetailsManager(List.of(u1, u2));
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 
 }
