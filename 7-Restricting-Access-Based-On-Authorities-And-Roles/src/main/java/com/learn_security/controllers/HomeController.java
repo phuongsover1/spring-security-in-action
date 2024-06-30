cb0f21ce7def78c2c3b039be6ad3a04176f7515d
Phuong Nguyen <phuongsover2@gmail.com>
2024-06-30 16:17:57 +0700

    Changing controller to restcontroller annotation

--- 7-Restricting-Access-Based-On-Authorities-And-Roles/src/main/java/com/learn_security/controllers/HomeController.java
+++ 7-Restricting-Access-Based-On-Authorities-And-Roles/src/main/java/com/learn_security/controllers/HomeController.java
@@ -3,8 +3,9 @@ package com.learn_security.controllers;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @Slf4j
 public class HomeController {
 
