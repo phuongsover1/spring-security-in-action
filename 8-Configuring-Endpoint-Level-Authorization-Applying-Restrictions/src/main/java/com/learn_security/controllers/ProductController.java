accf6a72f087cf4771df4a5d3fa3b62cf4f2a3ad
Phuong Nguyen <phuongsover2@gmail.com>
2024-06-30 21:18:50 +0700

    Listing 8.10 The definition of an endpoint with a path variable in a controller class

---
+++ 8-Configuring-Endpoint-Level-Authorization-Applying-Restrictions/src/main/java/com/learn_security/controllers/ProductController.java
@@ -0,0 +1,13 @@
+package com.learn_security.controllers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class ProductController {
+    @GetMapping("/product/{code}")
+    public String productCode(@PathVariable String code) {
+        return code;
+    }
+}
