2e7fcdc1dc9bd0f45d06cc0e706d61c1ba354195
Phuong Nguyen <phuongsover2@gmail.com>
2024-06-30 16:09:03 +0700

    Listing 7.4 Making all the endpoints accessible for everyone without authentication

--- 7-Restricting-Access-Based-On-Authorities-And-Roles/src/main/java/com/learn_security/config/ProjectConfig.java
+++ 7-Restricting-Access-Based-On-Authorities-And-Roles/src/main/java/com/learn_security/config/ProjectConfig.java
@@ -2,12 +2,15 @@ package com.learn_security.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 import java.util.List;
 
@@ -32,6 +35,17 @@ public class ProjectConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http)
+            throws Exception {
+
+        http.httpBasic(Customizer.withDefaults());
+
+        http.authorizeHttpRequests(c -> c.anyRequest().permitAll());
+
+        return http.build();
     }
 
 }
