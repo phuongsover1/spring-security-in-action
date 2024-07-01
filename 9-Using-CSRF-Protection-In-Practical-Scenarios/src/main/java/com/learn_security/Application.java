2f1dfa1d6d6fddeefdbc4b9658b0425614c5ef49
Phuong Nguyen <phuongsover2@gmail.com>
2024-07-01 22:19:24 +0700

    init project

---
+++ 9-Using-CSRF-Protection-In-Practical-Scenarios/src/main/java/com/learn_security/Application.java
@@ -0,0 +1,14 @@
+package com.learn_security;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
