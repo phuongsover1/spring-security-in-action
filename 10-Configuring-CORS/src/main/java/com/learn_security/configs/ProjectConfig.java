ffcb569af2313d7bd17e7bbf7fbd9d14add0347c
Phuong Nguyen <phuongsover2@gmail.com>
2024-07-01 23:31:59 +0700

    Listing 10.2 The definition of the configuration class

---
+++ 10-Configuring-CORS/src/main/java/com/learn_security/configs/ProjectConfig.java
@@ -0,0 +1,18 @@
+package com.learn_security.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ProjectConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.csrf(c -> c.disable());
+
+        http.authorizeHttpRequests(c -> c.anyRequest().permitAll());
+
+        return http.build();
+    }
+}
