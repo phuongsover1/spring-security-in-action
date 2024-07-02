fcf996d78fce091f2c0867e3054cbccb3c796cff
Phuong Nguyen <phuongsover2@gmail.com>
2024-07-03 00:46:40 +0700

    Init project

---
+++ 11-Enabling-method-security-in-your-project/src/main/java/com/learn_security/configs/ProjectConfig.java
@@ -0,0 +1,15 @@
+package com.learn_security.configs;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+
+import java.util.List;
+
+@Configuration
+public class ProjectConfig {
+
+}
