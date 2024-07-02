a7e707bcadd8e348b0e3e76eafd359a04a7a97a9
Phuong Nguyen <phuongsover2@gmail.com>
2024-07-03 01:01:27 +0700

    Listing 11.2 The configuration class for UserDetailsService and PasswordEncoder

--- 11-Enabling-method-security-in-your-project/src/main/java/com/learn_security/configs/ProjectConfig.java
+++ 11-Enabling-method-security-in-your-project/src/main/java/com/learn_security/configs/ProjectConfig.java
@@ -3,15 +3,39 @@ package com.learn_security.configs;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.web.SecurityFilterChain;
-import org.springframework.web.cors.CorsConfiguration;
-import org.springframework.web.cors.CorsConfigurationSource;
-
-import java.util.List;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 
 @Configuration
 @EnableMethodSecurity
 public class ProjectConfig {
+    @Bean
+    public UserDetailsService userDetailsService() {
+        var service = new InMemoryUserDetailsManager();
+
+        var u1 = User.withUsername("natalie")
+                .password("12345")
+                .authorities("READ")
+                .build();
+
+        var u2 = User.withUsername("emma")
+                .password("12345")
+                .authorities("WRITE")
+                .build();
+
+        service.createUser(u1);
+        service.createUser(u2);
+
+        return service;
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
 
 }
