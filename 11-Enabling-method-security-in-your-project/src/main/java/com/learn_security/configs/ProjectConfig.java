24d87fcf38f8384c4a6e504b4c8a282091772eaf
Phuong Nguyen <phuongsover2@gmail.com>
2024-07-03 00:47:04 +0700

    Listing 11.1 Enabling method security

--- 11-Enabling-method-security-in-your-project/src/main/java/com/learn_security/configs/ProjectConfig.java
+++ 11-Enabling-method-security-in-your-project/src/main/java/com/learn_security/configs/ProjectConfig.java
@@ -2,6 +2,7 @@ package com.learn_security.configs;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.web.cors.CorsConfiguration;
@@ -10,6 +11,7 @@ import org.springframework.web.cors.CorsConfigurationSource;
 import java.util.List;
 
 @Configuration
+@EnableMethodSecurity
 public class ProjectConfig {
 
 }
