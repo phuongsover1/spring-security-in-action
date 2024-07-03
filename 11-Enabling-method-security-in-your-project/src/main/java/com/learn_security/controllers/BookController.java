dfd649257e7672fa55ca4f0152eb49a1b1ac8149
Phuong Nguyen <phuongsover2@gmail.com>
2024-07-04 01:05:30 +0700

    Checking parameter name with authenticated username sent the request

--- 11-Enabling-method-security-in-your-project/src/main/java/com/learn_security/controllers/BookController.java
+++ 11-Enabling-method-security-in-your-project/src/main/java/com/learn_security/controllers/BookController.java
@@ -2,6 +2,7 @@ package com.learn_security.controllers;
 
 import com.learn_security.models.Employee;
 import com.learn_security.services.BookService;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -14,6 +15,7 @@ public class BookController {
         this.bookService = bookService;
     }
 
+    @PreAuthorize("#name == authentication.principal.username")
     @GetMapping("/book/details/{name}")
     public Employee getDetails(@PathVariable String name) {
         return bookService.getBookDetails(name);
