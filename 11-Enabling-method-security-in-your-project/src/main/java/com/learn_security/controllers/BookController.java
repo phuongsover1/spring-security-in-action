fa9159ded61a1008bcadd0c529d3f4a3380eaee4
Phuong Nguyen <phuongsover2@gmail.com>
2024-07-04 00:55:12 +0700

    Listing 11.10 The controller class implementing the endpoint

---
+++ 11-Enabling-method-security-in-your-project/src/main/java/com/learn_security/controllers/BookController.java
@@ -0,0 +1,21 @@
+package com.learn_security.controllers;
+
+import com.learn_security.models.Employee;
+import com.learn_security.services.BookService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BookController {
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    @GetMapping("/book/details/{name}")
+    public Employee getDetails(@PathVariable String name) {
+        return bookService.getBookDetails(name);
+    }
+}
