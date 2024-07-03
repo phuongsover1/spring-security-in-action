6b5d3acbe1abb9b15d89589c96ddfd81f8576583
Phuong Nguyen <phuongsover2@gmail.com>
2024-07-03 23:59:54 +0700

    Listing 11.5 The controller class defining an endpoint for testing

--- 11-Enabling-method-security-in-your-project/src/main/java/com/learn_security/controllers/HelloController.java
+++ 11-Enabling-method-security-in-your-project/src/main/java/com/learn_security/controllers/HelloController.java
@@ -2,7 +2,10 @@ package com.learn_security.controllers;
 
 import com.learn_security.services.NameService;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class HelloController {
@@ -16,5 +19,10 @@ public class HelloController {
     @GetMapping("/hello")
     public String hello() {
         return "Hello, " + nameService.getName();
+    }
+
+    @GetMapping("/secret/names/{name}")
+    public List<String> names(@PathVariable String name) {
+        return nameService.getSecretNames(name);
     }
 }
