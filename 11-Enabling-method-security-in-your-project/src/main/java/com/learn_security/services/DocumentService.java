26389cf660178449942c33fa738fc095ecdb779e
Phuong Nguyen <phuongsover2@gmail.com>
2024-07-05 00:17:37 +0700

    Listing 11.13 The DocumentService class implementing the protected method

---
+++ 11-Enabling-method-security-in-your-project/src/main/java/com/learn_security/services/DocumentService.java
@@ -0,0 +1,21 @@
+package com.learn_security.services;
+
+import com.learn_security.models.Document;
+import com.learn_security.repositories.DocumentRepository;
+import org.springframework.security.access.prepost.PostAuthorize;
+import org.springframework.stereotype.Service;
+
+@Service
+public class DocumentService {
+    private final DocumentRepository documentRepository;
+
+    public DocumentService(DocumentRepository documentRepository) {
+        this.documentRepository = documentRepository;
+    }
+
+    @PostAuthorize
+            ("hasPermission(returnObject, 'ROLE_admin')")
+    public Document getDocument(String code) {
+        return documentRepository.findDocument(code);
+    }
+}
