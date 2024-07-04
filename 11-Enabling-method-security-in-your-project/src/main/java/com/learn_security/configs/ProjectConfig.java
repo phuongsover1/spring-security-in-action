7ebf68d42710241d9a08359a8f1470a10f29b0c1
Phuong Nguyen <phuongsover2@gmail.com>
2024-07-05 00:26:16 +0700

    Listing 11.16 Configuring the PermissionEvaluator in the configuration class

--- 11-Enabling-method-security-in-your-project/src/main/java/com/learn_security/configs/ProjectConfig.java
+++ 11-Enabling-method-security-in-your-project/src/main/java/com/learn_security/configs/ProjectConfig.java
@@ -1,7 +1,10 @@
 package com.learn_security.configs;
 
+import com.learn_security.authorization.rules.DocumentPermissionEvaluator;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.access.expression.method.DefaultMethodSecurityExpressionHandler;
+import org.springframework.security.access.expression.method.MethodSecurityExpressionHandler;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetailsService;
@@ -12,6 +15,21 @@ import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 @Configuration
 @EnableMethodSecurity
 public class ProjectConfig {
+    private final DocumentPermissionEvaluator evaluator;
+
+    public ProjectConfig(DocumentPermissionEvaluator evaluator) {
+        this.evaluator = evaluator;
+    }
+
+    @Bean
+    protected MethodSecurityExpressionHandler createExpressionHandler() {
+        var expressionHandler = new DefaultMethodSecurityExpressionHandler();
+
+        expressionHandler.setPermissionEvaluator(evaluator);
+
+        return expressionHandler;
+    }
+
     @Bean
     public UserDetailsService userDetailsService() {
         var service = new InMemoryUserDetailsManager();
