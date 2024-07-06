15f2a1994653a0efdd4e2594e578f989bfb80b45
Phuong Nguyen <phuongsover2@gmail.com>
2024-07-07 00:55:34 +0700

    Adding logging in CustomCsrfTokenRepository

--- 9.1-Customizing-CSRF/src/main/java/com/learn_security/repositories/CustomCsrfTokenRepository.java
+++ 9.1-Customizing-CSRF/src/main/java/com/learn_security/repositories/CustomCsrfTokenRepository.java
@@ -10,10 +10,13 @@ import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 import java.util.UUID;
+import java.util.logging.Logger;
 
 @Component
 public class CustomCsrfTokenRepository implements CsrfTokenRepository {
     private final JpaTokenRepository tokenRepository;
+
+    private final Logger logger = Logger.getLogger(CustomCsrfTokenRepository.class.getName());
 
     public CustomCsrfTokenRepository(JpaTokenRepository tokenRepository) {
         this.tokenRepository = tokenRepository;
@@ -21,12 +24,15 @@ public class CustomCsrfTokenRepository implements CsrfTokenRepository {
 
     @Override
     public CsrfToken generateToken(HttpServletRequest request) {
+        logger.info("Inside generateToken");
+
         String uuid = UUID.randomUUID().toString();
         return new DefaultCsrfToken("X-CSRF-TOKEN", "_csrf", uuid);
     }
 
     @Override
     public void saveToken(CsrfToken csrfToken, HttpServletRequest request, HttpServletResponse response) {
+        logger.info("Inside saveToken");
         String identifier = request.getHeader("X-IDENTIFIER");
 
         Optional<Token> existingToken = tokenRepository.findTokenByIdentifier(identifier);
@@ -46,6 +52,7 @@ public class CustomCsrfTokenRepository implements CsrfTokenRepository {
 
     @Override
     public CsrfToken loadToken(HttpServletRequest request) {
+        logger.info("Inside loadToken");
         String identifier = request.getHeader("X-IDENTIFIER");
 
         Optional<Token> existingToken = tokenRepository.findTokenByIdentifier(identifier);
