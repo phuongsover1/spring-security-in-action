ece6621f51c42dfef60ff2c4de15218ab1848017
Phuong Nguyen <phuongsover2@gmail.com>
2024-07-06 14:34:46 +0700

    Listing 12.9 The Product entity class

--- 12-Filtering-in-Spring-Data-repositories/src/main/java/com/learn_security/models/Product.java
+++ 12-Filtering-in-Spring-Data-repositories/src/main/java/com/learn_security/models/Product.java
@@ -1,8 +1,20 @@
 package com.learn_security.models;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
 public class Product {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
     private String name;
     private String owner;
+
+    public Product() {
+    }
 
     public Product(String name, String owner) {
         this.name = name;
