a46d8932143cbbf4751f614bb1615ee08bac46be
Phuong Nguyen <phuongsover2@gmail.com>
2024-07-06 15:37:52 +0700

    Listing 12.13 Using SpEL in the query in the repository interface

--- 12-Filtering-in-Spring-Data-repositories/src/main/java/com/learn_security/repositories/ProductRepository.java
+++ 12-Filtering-in-Spring-Data-repositories/src/main/java/com/learn_security/repositories/ProductRepository.java
@@ -2,6 +2,7 @@ package com.learn_security.repositories;
 
 import com.learn_security.models.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.security.access.prepost.PostFilter;
 import org.springframework.stereotype.Repository;
 
@@ -10,6 +11,12 @@ import java.util.List;
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Integer> {
 
-    @PostFilter("filterObject.owner == authentication.name")
+//    @PostFilter("filterObject.owner == authentication.name")
+
+    @Query("""
+                    SELECT p FROM Product p WHERE 
+                    p.name LIKE %:text% AND 
+                    p.owner =?#{authentication.name}
+            """)
     List<Product> findProductByNameContains(String text);
 }
