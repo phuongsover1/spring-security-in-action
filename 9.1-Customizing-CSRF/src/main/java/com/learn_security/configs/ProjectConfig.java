795f9195fbe384b4776bc56a731982d21dab38a4
Phuong Nguyen <phuongsover2@gmail.com>
2024-07-06 22:32:07 +0700

    Listing 9.18 The configuration class for the custom CsrfTokenRepository

--- 9.1-Customizing-CSRF/src/main/java/com/learn_security/configs/ProjectConfig.java
+++ 9.1-Customizing-CSRF/src/main/java/com/learn_security/configs/ProjectConfig.java
@@ -1,5 +1,6 @@
 package com.learn_security.configs;
 
+import com.learn_security.repositories.CustomCsrfTokenRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -8,9 +9,16 @@ import org.springframework.security.web.SecurityFilterChain;
 @Configuration
 public class ProjectConfig {
 
+    private final CustomCsrfTokenRepository customCsrfTokenRepository;
+
+    public ProjectConfig(CustomCsrfTokenRepository csrfTokenRepository) {
+        this.customCsrfTokenRepository = csrfTokenRepository;
+    }
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(c -> {
+            c.csrfTokenRepository(customCsrfTokenRepository);
             c.ignoringRequestMatchers("/ciao");
         });
 
