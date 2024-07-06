6b4b9243f62277d52048b3d0bedf853cd5e20409
Phuong Nguyen <phuongsover2@gmail.com>
2024-07-07 00:54:18 +0700

    Needing to inject CsrfToken into request GET method in order to generate or check exist token and return csrf token for get request

--- 9.1-Customizing-CSRF/src/main/java/com/learn_security/configs/ProjectConfig.java
+++ 9.1-Customizing-CSRF/src/main/java/com/learn_security/configs/ProjectConfig.java
@@ -4,10 +4,12 @@ import com.learn_security.repositories.CustomCsrfTokenRepository;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 
 @Configuration
+@EnableWebSecurity
 public class ProjectConfig {
 
     private final CustomCsrfTokenRepository customCsrfTokenRepository;
@@ -20,8 +22,9 @@ public class ProjectConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(c -> {
             c.csrfTokenRepository(customCsrfTokenRepository);
-            c.csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler());
-            c.ignoringRequestMatchers("/ciao");
+            c.csrfTokenRequestHandler(
+                    new CsrfTokenRequestAttributeHandler()
+            );
         });
 
         http.authorizeHttpRequests(c -> c.anyRequest().permitAll());
--- 9.1-Customizing-CSRF/src/main/java/com/learn_security/controllers/HelloController.java
+++ 9.1-Customizing-CSRF/src/main/java/com/learn_security/controllers/HelloController.java
@@ -1,5 +1,10 @@
 package com.learn_security.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -8,9 +13,14 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloController {
 
     @GetMapping("/hello")
-    public String getHello() {
-        return "GET Hello!";
+    public ResponseEntity<String> getHello(CsrfToken token, HttpServletRequest request) {
+
+        HttpHeaders headers = new HttpHeaders();
+        headers.add("X-CSRF-TOKEN", token.getToken());
+        headers.add("X-IDENTIFIER", request.getHeader("X-IDENTIFIER"));
+        return new ResponseEntity<>("GET Hello!", headers, HttpStatus.OK);
     }
+
     @PostMapping("/hello")
     public String postHello() {
         return "Post Hello!";
