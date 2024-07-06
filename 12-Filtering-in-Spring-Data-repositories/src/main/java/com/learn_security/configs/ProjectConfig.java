6e13d701188a467c008d7fda76fd63963f8b9d20
Phuong Nguyen <phuongsover2@gmail.com>
2024-07-06 15:36:17 +0700

    Added Security Data to provide SecurityEvaluationContextExtension class for using SpEL language in @Query

--- 12-Filtering-in-Spring-Data-repositories/src/main/java/com/learn_security/configs/ProjectConfig.java
+++ 12-Filtering-in-Spring-Data-repositories/src/main/java/com/learn_security/configs/ProjectConfig.java
@@ -10,12 +10,18 @@ import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.data.repository.query.SecurityEvaluationContextExtension;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableMethodSecurity
 public class ProjectConfig {
+
+    @Bean
+    public SecurityEvaluationContextExtension securityEvaluationContextExtension() {
+        return new SecurityEvaluationContextExtension();
+    }
     @Bean
     public UserDetailsService userDetailsService() {
         var uds = new InMemoryUserDetailsManager();
