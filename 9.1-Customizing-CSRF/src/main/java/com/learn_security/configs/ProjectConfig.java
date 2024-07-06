09925f64c5134f9fb3f92c8ade0332df0fbf5cf3
Phuong Nguyen <phuongsover2@gmail.com>
2024-07-06 22:40:15 +0700

    Listing 9.19 The configuration class for the custom CsrfTokenRepository

--- 9.1-Customizing-CSRF/src/main/java/com/learn_security/configs/ProjectConfig.java
+++ 9.1-Customizing-CSRF/src/main/java/com/learn_security/configs/ProjectConfig.java
@@ -5,6 +5,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 
 @Configuration
 public class ProjectConfig {
@@ -19,6 +20,7 @@ public class ProjectConfig {
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.csrf(c -> {
             c.csrfTokenRepository(customCsrfTokenRepository);
+            c.csrfTokenRequestHandler(new CsrfTokenRequestAttributeHandler());
             c.ignoringRequestMatchers("/ciao");
         });
 
--- 9.1-Customizing-CSRF/src/main/java/com/learn_security/controllers/HelloController.java
+++ 9.1-Customizing-CSRF/src/main/java/com/learn_security/controllers/HelloController.java
@@ -1,10 +1,16 @@
 package com.learn_security.controllers;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloController {
+
+    @GetMapping("/hello")
+    public String getHello() {
+        return "GET Hello!";
+    }
     @PostMapping("/hello")
     public String postHello() {
         return "Post Hello!";
