0a3cde396064e03ff81df8bd67003173d7261509
Phuong Nguyen <phuongsover2@gmail.com>
2024-07-06 22:02:48 +0700

    Listing 9.12 The definition of the JPA entity class

---
+++ 9.1-Customizing-CSRF/src/main/java/com/learn_security/models/Token.java
@@ -0,0 +1,38 @@
+package com.learn_security.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Token {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String identifier;
+
+    private String token;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public void setIdentifier(String identifier) {
+        this.identifier = identifier;
+    }
+
+    public String getToken() {
+        return token;
+    }
+
+    public void setToken(String token) {
+        this.token = token;
+    }
+}
