80f9f0f5b76ab0b77c4381e105ccf4621ec5d0a3
Phuong Nguyen <phuongsover2@gmail.com>
2024-07-09 00:11:53 +0700

    Listing 14.4 Implementing client details management

---
+++ 14-Minimal-Authorization-Server/src/main/java/com/learn_security/authorization/configurations/ClientManagementConfig.java
@@ -0,0 +1,35 @@
+package com.learn_security.authorization.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.core.oidc.OidcScopes;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+
+import java.util.UUID;
+
+@Configuration
+public class ClientManagementConfig {
+    @Bean
+    public RegisteredClientRepository registeredClientRepository() {
+        RegisteredClient registeredClient =
+                RegisteredClient.withId(UUID.randomUUID().toString())
+                        .clientId("client")
+                        .clientSecret("secret")
+                        .clientAuthenticationMethod(
+                                ClientAuthenticationMethod.CLIENT_SECRET_BASIC
+                        )
+                        .authorizationGrantType(
+                                AuthorizationGrantType.AUTHORIZATION_CODE
+                        )
+                        .redirectUri("https://www.manning.com/authorized")
+                        .scope(OidcScopes.OPENID)
+                        .build();
+
+        return new InMemoryRegisteredClientRepository(registeredClient);
+    }
+
+}
