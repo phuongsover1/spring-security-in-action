dd0b0fea34713107791a8ca9c7a1ad02733ab5c6
Phuong Nguyen <phuongsover2@gmail.com>
2024-07-10 00:36:42 +0700

    Listing 15.5 Configuring the authentication with JWTs

--- 15-Configuring-Resource-Server/src/main/java/com/learn_security/authorization/configurations/ProjectConfig.java
+++ 15-Configuring-Resource-Server/src/main/java/com/learn_security/authorization/configurations/ProjectConfig.java
@@ -1,10 +1,24 @@
 package com.learn_security.authorization.configurations;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ProjectConfig {
     @Value("${keySetURI}")
     private String keySetURI;
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+
+        http.oauth2ResourceServer(
+                c -> c.jwt(
+                        j -> j.jwkSetUri(keySetURI)
+                )
+        );
+        return http.build();
+    }
 }
