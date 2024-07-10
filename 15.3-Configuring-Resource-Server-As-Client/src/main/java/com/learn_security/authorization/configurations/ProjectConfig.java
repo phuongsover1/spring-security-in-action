4c731ed42e0a898b1a2bbe9c4c60b23b0e10d1e8
Phuong Nguyen <phuongsover2@gmail.com>
2024-07-11 00:46:26 +0700

    Listing 15.14 Injecting the values in fields of the configuration class

--- 15.3-Configuring-Resource-Server-As-Client/src/main/java/com/learn_security/authorization/configurations/ProjectConfig.java
+++ 15.3-Configuring-Resource-Server-As-Client/src/main/java/com/learn_security/authorization/configurations/ProjectConfig.java
@@ -1,5 +1,6 @@
 package com.learn_security.authorization.configurations;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -7,6 +8,16 @@ import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ProjectConfig {
+    @Value("${introspectionURI}")
+    private String instropectionUri;
+
+    @Value("${resourceserver.clientID}")
+    private String resourceServerClientID;
+
+    @Value("${resourceserver.secret}")
+    private String resourceServerClientSecret;
+
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
