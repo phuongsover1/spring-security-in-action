020e24718e35873e9c46b93a27523201f61aa2e4
Phuong Nguyen <phuongsover2@gmail.com>
2024-07-10 22:59:15 +0700

    Listing 15.8 Defining a custom authentication object

---
+++ 15-Configuring-Resource-Server/src/main/java/com/learn_security/authorization/configurations/CustomAuthentication.java
@@ -0,0 +1,21 @@
+package com.learn_security.authorization.configurations;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.oauth2.jwt.Jwt;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationToken;
+
+import java.util.Collection;
+
+public class CustomAuthentication extends JwtAuthenticationToken {
+    private final String priority;
+
+    public CustomAuthentication(Jwt jwt, Collection<? extends GrantedAuthority> authorities,
+                                String priority) {
+        super(jwt, authorities);
+        this.priority = priority;
+    }
+
+    public String getPriority() {
+        return priority;
+    }
+}
