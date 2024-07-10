57df908bf0eafbd214c49317a13023f1ec688c47
Phuong Nguyen <phuongsover2@gmail.com>
2024-07-10 23:21:33 +0700

    Listing 15.11 The /demo endpoint response contains the priority field

--- 15-Configuring-Resource-Server/src/main/java/com/learn_security/authorization/controllers/DemoController.java
+++ 15-Configuring-Resource-Server/src/main/java/com/learn_security/authorization/controllers/DemoController.java
@@ -1,5 +1,7 @@
 package com.learn_security.authorization.controllers;
 
+import com.learn_security.authorization.configurations.CustomAuthentication;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,7 +9,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class DemoController {
 
     @GetMapping("/demo")
-    public String demo() {
-        return "Demo";
+    public Authentication demo(Authentication a) {
+        return a;
     }
 }
