17e9c0ea7385f675a7dc267e15513db323b68c47
Phuong Nguyen <phuongsover2@gmail.com>
2024-07-11 00:32:30 +0700

    Listing 15.12 The RegisteredClientRepository definition

--- 15.3-Configuring-Authorization-Server-Accept-Resource-Server-Client/src/main/java/com/learn_security/authorization/configurations/ClientManagementConfig.java
+++ 15.3-Configuring-Authorization-Server-Accept-Resource-Server-Client/src/main/java/com/learn_security/authorization/configurations/ClientManagementConfig.java
@@ -1,8 +1,53 @@
 package com.learn_security.authorization.configurations;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.oauth2.core.AuthorizationGrantType;
+import org.springframework.security.oauth2.core.ClientAuthenticationMethod;
+import org.springframework.security.oauth2.server.authorization.client.InMemoryRegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClient;
+import org.springframework.security.oauth2.server.authorization.client.RegisteredClientRepository;
+import org.springframework.security.oauth2.server.authorization.settings.OAuth2TokenFormat;
+import org.springframework.security.oauth2.server.authorization.settings.TokenSettings;
+
+import java.time.Duration;
+import java.util.UUID;
 
 @Configuration
 public class ClientManagementConfig {
 
+    @Bean
+    public RegisteredClientRepository registeredClientRepository() {
+        RegisteredClient registeredClient =
+                RegisteredClient.withId(UUID.randomUUID().toString())
+                        .clientId("client")
+                        .clientSecret("secret")
+                        .clientAuthenticationMethod(
+                                ClientAuthenticationMethod.CLIENT_SECRET_BASIC
+                        )
+                        .authorizationGrantType(
+                                AuthorizationGrantType.CLIENT_CREDENTIALS
+                        )
+                        .tokenSettings(TokenSettings.builder()
+                                .accessTokenFormat(OAuth2TokenFormat.REFERENCE)
+                                .accessTokenTimeToLive(Duration.ofHours(1))
+                                .build())
+                        .scope("CUSTOM")
+                        .build();
+
+        RegisteredClient resourceServer =
+                RegisteredClient.withId(UUID.randomUUID().toString())
+                        .clientId("resource_server")
+                        .clientSecret("resource_server_secret")
+                        .clientAuthenticationMethod(
+                                ClientAuthenticationMethod.CLIENT_SECRET_BASIC
+                        )
+                        .authorizationGrantType(
+                                AuthorizationGrantType.CLIENT_CREDENTIALS
+                        )
+                        .build();
+
+        return new InMemoryRegisteredClientRepository(registeredClient, resourceServer);
+
+    }
 }
