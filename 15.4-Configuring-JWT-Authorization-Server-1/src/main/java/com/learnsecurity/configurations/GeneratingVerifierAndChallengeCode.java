84b323d260fa8d1f03b3313ab0005920236449e3
Phuong Nguyen <phuongsover2@gmail.com>
2024-07-12 01:21:14 +0700

    Generating the code verifier and challenge for authorization_server_1

---
+++ 15.4-Configuring-JWT-Authorization-Server-1/src/main/java/com/learnsecurity/configurations/GeneratingVerifierAndChallengeCode.java
@@ -0,0 +1,35 @@
+package com.learnsecurity.configurations;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import java.security.MessageDigest;
+import java.security.SecureRandom;
+import java.util.Base64;
+import java.util.logging.Logger;
+
+@Component
+public class GeneratingVerifierAndChallengeCode implements ApplicationRunner {
+    private Logger logger = Logger.getLogger(GeneratingVerifierAndChallengeCode.class.getName());
+
+    @Override
+    public void run(ApplicationArguments args) throws Exception {
+        SecureRandom secureRandom = new SecureRandom();
+        byte[] code = new byte[32];
+        secureRandom.nextBytes(code);
+        String codeVerifier = Base64.getUrlEncoder()
+                .withoutPadding()
+                .encodeToString(code);
+
+        logger.info("Authorization Server 1 Verifier code: " + codeVerifier);
+
+        MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+        byte[] digest = messageDigest.digest(codeVerifier.getBytes());
+        String codeChallenge = Base64.getUrlEncoder()
+                .withoutPadding()
+                .encodeToString(digest);
+        logger.info("Authorization Server 1 Challenge code: " + codeChallenge);
+
+    }
+}
