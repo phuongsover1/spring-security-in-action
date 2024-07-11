c5b56d6c6654dd39f12d2c2ace8bf13573f27e28
Phuong Nguyen <phuongsover2@gmail.com>
2024-07-11 22:51:03 +0700

    Listing 15.18 Using both JWT and opaque tokens

--- 15.Resource-Server-Works-With-Multiple-Authorization-Server/src/main/java/com/learnsecurity/configurations/ProjectConfig.java
+++ 15.Resource-Server-Works-With-Multiple-Authorization-Server/src/main/java/com/learnsecurity/configurations/ProjectConfig.java
@@ -1,20 +1,43 @@
 package com.learnsecurity.configurations;
 
 import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationManagerResolver;
+import org.springframework.security.authentication.ProviderManager;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.oauth2.server.resource.authentication.JwtIssuerAuthenticationManagerResolver;
+import org.springframework.security.oauth2.jwt.JwtDecoder;
+import org.springframework.security.oauth2.jwt.NimbusJwtDecoder;
+import org.springframework.security.oauth2.server.resource.authentication.JwtAuthenticationProvider;
+import org.springframework.security.oauth2.server.resource.authentication.OpaqueTokenAuthenticationProvider;
+import org.springframework.security.oauth2.server.resource.introspection.OpaqueTokenIntrospector;
+import org.springframework.security.oauth2.server.resource.introspection.SpringOpaqueTokenIntrospector;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class ProjectConfig {
 
+    @Value("${jwkSetURI}")
+    private String jwkSetURI;
+
+    @Value("${introspectionURI}")
+    private String introspectionURI;
+
+    @Value("${clientID}")
+    private String clientID;
+
+    @Value("${clientSecret}")
+    private String clientSecret;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.oauth2ResourceServer(
-                j -> j.authenticationManagerResolver(authenticationManagerResolver())
+                j -> j.authenticationManagerResolver(authenticationManagerResolver(
+                        jwtDecoder(),
+                        opaqueTokenIntrospector()
+                ))
         );
 
         http.authorizeHttpRequests(
@@ -25,12 +48,41 @@ public class ProjectConfig {
     }
 
     @Bean
-    public AuthenticationManagerResolver<HttpServletRequest> authenticationManagerResolver() {
-        var a = new JwtIssuerAuthenticationManagerResolver(
-                "http://localhost:7070",
-                "http://localhost:8080"
+    public AuthenticationManagerResolver<HttpServletRequest> authenticationManagerResolver(
+            JwtDecoder jwtDecoder,
+            OpaqueTokenIntrospector opaqueTokenIntrospector
+    ) {
 
+        AuthenticationManager jwtAuth = new ProviderManager(
+                new JwtAuthenticationProvider(jwtDecoder)
         );
-        return a;
+
+        AuthenticationManager opaqueAuth = new ProviderManager(
+                new OpaqueTokenAuthenticationProvider(opaqueTokenIntrospector)
+        );
+
+        return (request) -> {
+            if ("jwt".equals(request.getHeader("type"))) {
+                return jwtAuth;
+            } else {
+                return opaqueAuth;
+            }
+        };
+    }
+
+    @Bean
+    public JwtDecoder jwtDecoder() {
+        return NimbusJwtDecoder
+                .withJwkSetUri(jwkSetURI)
+                .build();
+    }
+
+    @Bean
+    public OpaqueTokenIntrospector opaqueTokenIntrospector() {
+        return new SpringOpaqueTokenIntrospector(
+                introspectionURI,
+                clientID,
+                clientSecret
+        );
     }
 }
