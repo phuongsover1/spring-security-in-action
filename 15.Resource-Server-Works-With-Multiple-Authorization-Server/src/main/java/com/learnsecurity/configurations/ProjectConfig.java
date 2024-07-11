03618e0b513fb7fbb565cdd781a9d77de812c92f
Phuong Nguyen <phuongsover2@gmail.com>
2024-07-11 22:17:44 +0700

    Listing 15.17 Working with two authorization servers that use JWT access tokens

---
+++ 15.Resource-Server-Works-With-Multiple-Authorization-Server/src/main/java/com/learnsecurity/configurations/ProjectConfig.java
@@ -0,0 +1,36 @@
+package com.learnsecurity.configurations;
+
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManagerResolver;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.server.resource.authentication.JwtIssuerAuthenticationManagerResolver;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ProjectConfig {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.oauth2ResourceServer(
+                j -> j.authenticationManagerResolver(authenticationManagerResolver())
+        );
+
+        http.authorizeHttpRequests(
+                c -> c.anyRequest().authenticated()
+        );
+
+        return http.build();
+    }
+
+    @Bean
+    public AuthenticationManagerResolver<HttpServletRequest> authenticationManagerResolver() {
+        var a = new JwtIssuerAuthenticationManagerResolver(
+                "http://localhost:7070",
+                "http://localhost:8080"
+
+        );
+        return a;
+    }
+}
