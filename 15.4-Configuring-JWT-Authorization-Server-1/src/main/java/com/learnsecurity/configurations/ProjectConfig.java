b571c84910e20a46416d3e5672ab83036256e7db
Phuong Nguyen <phuongsover2@gmail.com>
2024-07-12 00:41:52 +0700

    Implementing the filter for configuring protocol endpoints for authorization_server_1

---
+++ 15.4-Configuring-JWT-Authorization-Server-1/src/main/java/com/learnsecurity/configurations/ProjectConfig.java
@@ -0,0 +1,31 @@
+package com.learnsecurity.configurations;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configuration.OAuth2AuthorizationServerConfiguration;
+import org.springframework.security.oauth2.server.authorization.config.annotation.web.configurers.OAuth2AuthorizationServerConfigurer;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
+
+@Configuration
+public class ProjectConfig {
+    @Bean
+    @Order(1)
+    public SecurityFilterChain asFilter(HttpSecurity http) throws Exception {
+        OAuth2AuthorizationServerConfiguration
+                .applyDefaultSecurity(http);
+
+        http.getConfigurer(
+                OAuth2AuthorizationServerConfigurer.class
+        ).oidc(Customizer.withDefaults());
+
+        http.exceptionHandling(e -> e.authenticationEntryPoint(
+                new LoginUrlAuthenticationEntryPoint("/login")
+        ));
+
+        return http.build();
+    }
+}
