6bf88610a4912c14b5f66b273da7460f033e0b97
Phuong Nguyen <phuongsover2@gmail.com>
2024-07-17 00:35:27 +0700

    Listing 16.6 Obtaining the authentication details

--- 16.1-Configuring-Oauth2-Client/src/main/java/com/learnsecurity/controllers/HomeController.java
+++ 16.1-Configuring-Oauth2-Client/src/main/java/com/learnsecurity/controllers/HomeController.java
@@ -1,12 +1,22 @@
 package com.learnsecurity.controllers;
 
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.logging.Logger;
 
 @Controller
 public class HomeController {
+    private Logger logger = Logger.getLogger(HomeController.class.getName());
     @GetMapping("/")
-    public String home() {
+    public String home(OAuth2AuthenticationToken authentication, Model model) {
+        model.addAttribute("user", authentication.getPrincipal().getName());
+        logger.info("User " + authentication.getPrincipal().getName() + " logged in");
         return "index";
     }
 }
