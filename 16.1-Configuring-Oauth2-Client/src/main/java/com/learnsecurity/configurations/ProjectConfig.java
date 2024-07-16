72bc0ad47c51e25fbcd5c74744554335193cfea6
Phuong Nguyen <phuongsover2@gmail.com>
2024-07-16 23:59:15 +0700

    16.1.4 Adding flexibility (enable github,google based on configurations) to your configurations

--- 16.1-Configuring-Oauth2-Client/src/main/java/com/learnsecurity/configurations/ProjectConfig.java
+++ 16.1-Configuring-Oauth2-Client/src/main/java/com/learnsecurity/configurations/ProjectConfig.java
@@ -1,13 +1,37 @@
 package com.learnsecurity.configurations;
 
+import ch.qos.logback.core.net.server.Client;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+import org.springframework.security.oauth2.client.registration.ClientRegistration;
+import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
+import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
 import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Configuration
 public class ProjectConfig {
+    @Value("${GOOGLE_CLIENT_ID}")
+    private String googleClientId;
+
+    @Value("${GOOGLE_CLIENT_SECRET}")
+    private String googleClientSecret;
+
+    @Value("${GITHUB_CLIENT_ID}")
+    private String githubClientId;
+
+    @Value("${GITHUB_CLIENT_SECRET}")
+    private String githubClientSecret;
+
+    @Value("${enabled_oauth2_provider}")
+    private String enabledOauth2Providers;
+
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
         http.oauth2Login(Customizer.withDefaults());
@@ -15,5 +39,38 @@ public class ProjectConfig {
         http.authorizeHttpRequests(r -> r.anyRequest().authenticated());
 
         return http.build();
+    }
+
+    @Bean
+    public ClientRegistrationRepository clientRegistrationRepository() {
+        if (enabledOauth2Providers == null) {
+            return new InMemoryClientRegistrationRepository();
+        }
+        List<ClientRegistration>  clientRegistrationList = new ArrayList<>();
+        for (String providerId : enabledOauth2Providers.split(",")) {
+           if (providerId.equals("google")) {
+               clientRegistrationList.add(googleClientRegistration());
+           } else if (providerId.equals("github")) {
+               clientRegistrationList.add(githubClientRegistration());
+           }
+
+        }
+        return new InMemoryClientRegistrationRepository(clientRegistrationList);
+    }
+
+
+
+    private ClientRegistration googleClientRegistration() {
+        return CommonOAuth2Provider.GOOGLE.getBuilder("google")
+                .clientId(googleClientId)
+                .clientSecret(googleClientSecret)
+                .build();
+    }
+
+    private ClientRegistration githubClientRegistration() {
+        return CommonOAuth2Provider.GITHUB.getBuilder("github")
+                .clientId(githubClientId)
+                .clientSecret(githubClientSecret)
+                .build();
     }
 }
