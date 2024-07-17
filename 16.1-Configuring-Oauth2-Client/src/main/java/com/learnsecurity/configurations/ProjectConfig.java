dba47d54a247ca14a5dbe5bf112f8bd9a46f5408
Phuong Nguyen <phuongsover2@gmail.com>
2024-07-17 22:04:47 +0700

    Listing 16.10 Implementing an OAuth 2 client manager

--- 16.1-Configuring-Oauth2-Client/src/main/java/com/learnsecurity/configurations/ProjectConfig.java
+++ 16.1-Configuring-Oauth2-Client/src/main/java/com/learnsecurity/configurations/ProjectConfig.java
@@ -7,9 +7,13 @@ import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.oauth2.client.CommonOAuth2Provider;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientProviderBuilder;
 import org.springframework.security.oauth2.client.registration.ClientRegistration;
 import org.springframework.security.oauth2.client.registration.ClientRegistrationRepository;
 import org.springframework.security.oauth2.client.registration.InMemoryClientRegistrationRepository;
+import org.springframework.security.oauth2.client.web.DefaultOAuth2AuthorizedClientManager;
+import org.springframework.security.oauth2.client.web.OAuth2AuthorizedClientRepository;
 import org.springframework.security.oauth2.core.AuthorizationGrantType;
 import org.springframework.security.oauth2.core.oidc.OidcScopes;
 import org.springframework.security.web.SecurityFilterChain;
@@ -76,6 +80,26 @@ public class ProjectConfig {
                         .build();
 
         return new InMemoryClientRegistrationRepository(c1);
+    }
+
+    @Bean
+    @Profile("client-credentials")
+    public OAuth2AuthorizedClientManager oAuth2AuthorizedClientManager(
+            ClientRegistrationRepository clientRegistrationRepository,
+            OAuth2AuthorizedClientRepository auth2AuthorizedClientRepository
+    ) {
+        var provider = OAuth2AuthorizedClientProviderBuilder.builder()
+                .clientCredentials()
+                .build();
+
+        var cm = new DefaultOAuth2AuthorizedClientManager(
+                clientRegistrationRepository,
+                auth2AuthorizedClientRepository
+        );
+
+        cm.setAuthorizedClientProvider(provider);
+
+        return cm;
     }
 
 
