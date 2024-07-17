f00af0aa73a715124af10ec25248c0e2d8ef170d
Phuong Nguyen <phuongsover2@gmail.com>
2024-07-18 01:00:35 +0700

    Listing 17.4 Working with a ReactiveSecurityContextHolder

--- 17-Spring-Reactive-with-Security/src/main/java/com/learnsecurity/controllers/HelloController.java
+++ 17-Spring-Reactive-with-Security/src/main/java/com/learnsecurity/controllers/HelloController.java
@@ -1,6 +1,7 @@
 package com.learnsecurity.controllers;
 
 import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.ReactiveSecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -11,8 +12,11 @@ import java.net.Authenticator;
 public class HelloController {
 
     @GetMapping("/hello")
-    public Mono<String> hello(Mono<Authentication> auth) {
-        Mono<String> message = auth.map(a -> "Hello " + a.getName());
+    public Mono<String> hello() {
+        Mono<String> message = ReactiveSecurityContextHolder.getContext()
+                .map(ctx -> ctx.getAuthentication())
+                .map(auth -> "Hello " + auth.getName());
+
         return message;
     }
 }
