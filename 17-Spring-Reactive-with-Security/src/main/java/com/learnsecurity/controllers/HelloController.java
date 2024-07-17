89ffcf314c028be95f4dadc8ab8709c98f1ecc67
Phuong Nguyen <phuongsover2@gmail.com>
2024-07-18 00:47:11 +0700

    request the Authentication details by adding Mono<Authentication> as a parameter to the method in the controller class

--- 17-Spring-Reactive-with-Security/src/main/java/com/learnsecurity/controllers/HelloController.java
+++ 17-Spring-Reactive-with-Security/src/main/java/com/learnsecurity/controllers/HelloController.java
@@ -1,14 +1,18 @@
 package com.learnsecurity.controllers;
 
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
+
+import java.net.Authenticator;
 
 @RestController
 public class HelloController {
 
     @GetMapping("/hello")
-    public Mono<String> hello() {
-        return Mono.just("Hello");
+    public Mono<String> hello(Mono<Authentication> auth) {
+        Mono<String> message = auth.map(a -> "Hello " + a.getName());
+        return message;
     }
 }
