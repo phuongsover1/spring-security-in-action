046c267eaa7a47573533df89b8e94f0871ad127e
Phuong Nguyen <phuongsover2@gmail.com>
2024-07-22 20:37:39 +0700

    Listing 17.7 Configuring endpoint authorization for reactive apps

--- 17-Spring-Reactive-with-Security/src/main/java/com/learnsecurity/configurations/ProjectConfig.java
+++ 17-Spring-Reactive-with-Security/src/main/java/com/learnsecurity/configurations/ProjectConfig.java
@@ -2,13 +2,19 @@ package com.learnsecurity.configurations;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
 
 @Configuration
+@EnableWebFluxSecurity
 public class ProjectConfig {
 
     @Bean
@@ -26,5 +32,16 @@ public class ProjectConfig {
     @Bean
     public PasswordEncoder passwordEncoder() {
         return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) throws Exception {
+
+        http.httpBasic(Customizer.withDefaults());
+
+        http.authorizeExchange(c -> c.pathMatchers(HttpMethod.GET, "/hello").authenticated()
+                .anyExchange().permitAll());
+
+        return http.build();
     }
 }
