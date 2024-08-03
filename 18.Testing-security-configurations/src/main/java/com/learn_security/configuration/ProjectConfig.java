2ad727671bf5e37579b307391d7606c85422dcd0
Phuong Nguyen <phuongsover2@gmail.com>
2024-08-03 18:12:51 +0700

    Configuring controller

---
+++ 18.Testing-security-configurations/src/main/java/com/learn_security/configuration/ProjectConfig.java
@@ -0,0 +1,17 @@
+package com.learn_security.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class ProjectConfig {
+
+   @Bean
+   public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+      http.authorizeHttpRequests(c -> c.anyRequest().authenticated());
+
+      return http.build();
+   }
+}
