3df39a41a610aebd7d87cded0dcff48483d18ca9
Phuong Nguyen <phuongsover2@gmail.com>
2024-08-03 18:23:53 +0700

    Listing 18.4 Using @WithMockUser to define a mock authenticated user

--- 18.Testing-security-configurations/src/main/java/com/learn_security/configuration/ProjectConfig.java
+++ 18.Testing-security-configurations/src/main/java/com/learn_security/configuration/ProjectConfig.java
@@ -2,6 +2,7 @@ package com.learn_security.configuration;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -10,6 +11,7 @@ public class ProjectConfig {
 
    @Bean
    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+      http.httpBasic(Customizer.withDefaults());
       http.authorizeHttpRequests(c -> c.anyRequest().authenticated());
 
       return http.build();
--- 18.Testing-security-configurations/src/test/java/com/learn_security/MainTests.java
+++ 18.Testing-security-configurations/src/test/java/com/learn_security/MainTests.java
@@ -1,15 +1,30 @@
 package com.learn_security;
 
+import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.test.web.servlet.ResultMatcher.*;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.MockMvcBuilder.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+
+import java.net.http.HttpRequest;
+
 @SpringBootTest
 @AutoConfigureMockMvc
 public class MainTests {
-
    @Autowired
    private MockMvc mvc;
 
+   @Test
+   public void helloUnauthenticated() throws Exception {
+     mvc.perform(get("/hello"))
+         .andExpect(status().isUnauthorized());
+   }
 }
