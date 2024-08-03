003217cac56324035d231990800affa9ecbf6ee6
Phuong Nguyen <phuongsover2@gmail.com>
2024-08-03 18:00:39 +0700

    Listing 18.2 Adding MockMvc for implementing test scenarios

--- 18.Testing-security-configurations/src/test/java/com/learn_security/MainTests.java
+++ 18.Testing-security-configurations/src/test/java/com/learn_security/MainTests.java
@@ -1,8 +1,15 @@
 package com.learn_security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 public class MainTests {
+
+   @Autowired
+   private MockMvc mvc;
 
 }
